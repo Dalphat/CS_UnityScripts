20c34f6389e3bf3a8720108c6ad4b509e93201c3
Dalphat <dstatckflow@gmail.com>
2020-01-14 12:26:56 -0800

    Initial addition of LookAt script

---
+++ LookAt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    void Start()
+    {
+        if (!target)
+            Destroy(this);
+    }
+    private void FixedUpdate() => transform.LookAt(target);
+}
